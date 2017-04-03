7d0e95168a5e5e6af6063676ed1f366657df9020
ROOM1_8 <ROOM1_8@ROOM1_8-PC>
2017-04-03 20:30:44 +0900

    [Contents getter,setter]

--- e_nourriture/src/ent/model/dto/Contents.java
+++ e_nourriture/src/ent/model/dto/Contents.java
@@ -12,4 +12,60 @@ public class Contents {
 	private int contentsLikeCount; 			//좋아요
 	private ArrayList<Communication> communication;	//댓글
 	private ProductInfo productInfo;		//상품 정보
+	public String getContentsId() {
+		return ContentsId;
+	}
+	public void setContentsId(String contentsId) {
+		ContentsId = contentsId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	public String getImgURL() {
+		return imgURL;
+	}
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+	public int getContentsLikeCount() {
+		return contentsLikeCount;
+	}
+	public void setContentsLikeCount(int contentsLikeCount) {
+		this.contentsLikeCount = contentsLikeCount;
+	}
+	public ArrayList<Communication> getCommunication() {
+		return communication;
+	}
+	public void setCommunication(ArrayList<Communication> communication) {
+		this.communication = communication;
+	}
+	public ProductInfo getProductInfo() {
+		return productInfo;
+	}
+	public void setProductInfo(ProductInfo productInfo) {
+		this.productInfo = productInfo;
+	}
+	
+	
 }
