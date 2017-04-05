a574d8728f1195cdda1447f390a9b0d294ab0f5b
ROOM1_2 <ROOM1_2@ROOM1_2-PC>
2017-04-05 16:44:32 +0900

    [myContent]

--- e_nourriture/src/ent/model/dao/ContentsDAO.java
+++ e_nourriture/src/ent/model/dao/ContentsDAO.java
@@ -5,8 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import ent.model.dto.Contents;
+import ent.model.dto.ProductInfo;
 
 public class ContentsDAO {
 	
@@ -87,6 +89,72 @@ public class ContentsDAO {
 		}
 		
 		return 0;
+		
+	}
+	
+	// 2. 내 글 조회
+	
+	public ArrayList<Contents> myContents(String userid){
+		ArrayList<Contents> list = new ArrayList<Contents>();
+		
+		Connection conn=null;
+		Statement stmt=null;
+		Statement stmt2=null;
+		
+		String sql = "select contentsid, text, imgurl,times, contentslikecount from usercontents where userid='"+userid+"'";
+		String sql_product = "select productionname,productionprice,sellername,selleraddress,evaluation from contentsinfodb where contentsid=";
+		ResultSet rs =null;
+		ResultSet rs_product=null;
+		int contentsid=0;
+		String text=null;
+		String imgurl=null;
+		String times=null;
+		int contentslikecount=0;
+		try {
+			conn = getConnection();
+			stmt = getStatement(conn);
+			stmt2 = getStatement(conn);
+			rs = stmt.executeQuery(sql);
+			System.out.println(rs.next());
+			while(rs.next()){
+				contentsid = rs.getInt(1);
+				text=rs.getString(2);
+				imgurl=rs.getString(3);
+				times=rs.getString(4);
+				contentslikecount = rs.getInt(5);
+				System.out.println(contentsid+","+text+","+imgurl+","+times+","+contentslikecount);
+				Contents contents = new Contents();
+				contents.setContentId(contentsid+"");
+				contents.setContents(text);
+				contents.setImgURL(imgurl);
+				contents.setWriteDate(times);
+				contents.setContentsLikeCount(contentslikecount);
+				System.out.println(contents.toString());
+				
+				rs_product = stmt2.executeQuery(sql_product+contentsid);
+				if(rs_product.next()){
+					String productionName = rs_product.getString(1);
+					String productionprice = rs_product.getString(2);
+					String sellername = rs_product.getString(3);
+					String selleraddress = rs_product.getString(4);
+					int evaluation = rs_product.getInt(5);
+					ProductInfo productInfo = new ProductInfo();
+					productInfo.setProductName(productionName);
+					productInfo.setProductPrice(productionprice);
+					productInfo.setProductStore(sellername);
+					productInfo.setProductStoreAddr(selleraddress);
+					productInfo.setEvaluation(evaluation);
+					System.out.println(productInfo.toString());
+					contents.setProductInfo(productInfo);
+				}
+				list.add(contents);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
 		
 	}
 
